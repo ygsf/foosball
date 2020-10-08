b34ba25f3778d0d2a4eea21e05d274cdd964dac0
Jacob <yakov.gitlin@gmail.com>
2020-10-08 19:33:36 +1100

    write result to google sheet

--- foosball.py
+++ foosball.py
@@ -2,6 +2,16 @@
 import time
 import RPi.GPIO as GPIO
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("foosballsh").sheet1
+
+
 # choose BCM or BOARD numbering schemes.
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True) # TODO: might come in useful now.
@@ -13,9 +23,15 @@ GPIO_WHITE_CHANNEL = 18                 # GPIO channel for WHITE team IR receive
 def break_beam_callback(channel):
     if GPIO.input(GPIO_YELLOW_CHANNEL):
         print("Yellow Gool!")
+        cell = sheet.cell(2,2).value
+        print(cell)
+        cell = int(cell) + 1
+        print(cell)
+        sheet.update_cell(2,2,cell)
         return None
     if GPIO.input(GPIO_WHITE_CHANNEL):
         print("White Gool!")
+        sheet.update_cell(2,3,"4")
         return None
         
 # setup GPIO channels
